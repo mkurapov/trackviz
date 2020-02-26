453e391364e0aa7795f5922f6869756f2e0605fe
Max Kurapov <maxkurapov@gmail.com>
2020-02-25 20:22:26 -0700

    timestamp

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
 API_KEY = "56b54ab233380061bbdd39999aedef89";
 
 let usingDOM = true;
-
+let isRendered = false;
 var canvas = document.getElementById("c");
 var ctx = canvas.getContext("2d");
 var inputEl = document.getElementById("input-username");
-const statusEl = document.getElementById("status");
+var timestampEl = document.getElementById("timestamp");
+const setTimestamp = message => {
+  timestampEl.innerText = message;
+};
 
+window.addEventListener("load", () => inputEl.focus());
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+// let observer = new IntersectionObserver(
+//   (entries, observer) => {
+//     // console.log(observer);
+//     entries.forEach(entry => {
+//       console.log(entry.target.getAttribute("data-date"));
+//     });
+//   },
+//   { rootMargin: "100% 0px" }
+// );
+
+const statusEl = document.getElementById("status");
 const setStatus = message => {
   statusEl.innerText = message;
 };
 
 inputEl.addEventListener("keydown", ev => {
   if (ev.keyCode == 13 && ev.target.value !== username) {
-    changeUser(ev.target.value);
+    if (ev.target.value == "") {
+      clearRender();
+      setStatus("");
+    } else {
+      changeUser(ev.target.value);
+    }
   }
 });
 
@@ -27,7 +63,7 @@ let trackList = JSON.parse(localStorage.getItem("tracks")) || [];
 
 let loadedImages = [];
 let totalPages = 0;
-const MAX_PAGES = 10;
+const MAX_PAGES = 30;
 
 let newTracksToAdd = [];
 let mostRecentSavedTrack = trackList.length > 0 ? trackList[0] : null;
@@ -44,6 +80,11 @@ const fetchNow = (page = 1) => {
 
       for (let i = 0; i < data.recenttracks.track.length; i++) {
         let track = data.recenttracks.track[i];
+        if (!track.date) {
+          // happens if user is currently listening
+          continue;
+        }
+
         if (
           mostRecentSavedTrack &&
           mostRecentSavedTrack.date == track.date.uts
@@ -64,6 +105,7 @@ const fetchNow = (page = 1) => {
         setStatus(`Added ${newTracksToAdd.length} new tracks`);
         if (newTracksToAdd.length > 0) {
           trackList = [...newTracksToAdd, ...trackList];
+          localStorage.setItem("username", username);
           localStorage.setItem("tracks", JSON.stringify(trackList));
         }
 
@@ -116,26 +158,32 @@ const displayOnDOM = () => {
 
   let div = document.getElementById("viz");
   for (let i = 0; i < loadedImages.length; i++) {
+    // loadedImages[i].setAttribute("data-date", trackList[i].date);
     div.appendChild(loadedImages[i]);
-    // if (i > 0 && i % perRow == 0) {
-    //   row++;
-    // }
-    // console.log(`track ${i} of ${loadedImages.length} loaded.`);
+    // observer.observe(loadedImages[i]);
   }
+  setStatus(`Loaded ${loadedImages.length} tracks.`);
+  isRendered = true;
 };
 
 const clearRender = () => {
   usingDOM
     ? (document.getElementById("viz").innerHTML = "")
     : ctx.clearRect(0, 0, canvas.width, canvas.height);
+  isRendered = false;
 };
 
 const render = () => {
-  usingDOM ? displayOnDOM() : displayOnCanvas();
+  isRendered = false;
+  setStatus("Rendering tracks");
+  setTimeout(() => (usingDOM ? displayOnDOM() : displayOnCanvas()), 1000);
 };
 
 function windowResized() {
-  render();
+  // rerender if resized
+  if (isRendered) {
+    render();
+  }
 }
 
 let debouncedResize;
@@ -144,11 +192,26 @@ window.addEventListener("resize", () => {
   debouncedResize = setTimeout(windowResized, 200);
 });
 
+window.addEventListener("mousewheel", ev => {
+  let perc = getScrollPercent();
+  let itemIndex;
+  if (perc == 1) {
+    itemIndex = trackList.length - 1;
+  } else {
+    itemIndex = Math.floor(perc * trackList.length);
+  }
+  // let date = new Date();
+  // console.log(date);
+  let timestring = moment(trackList[itemIndex].date * 1000).format("MMMM YYYY");
+  // console.log(timestring);
+  setTimestamp(timestring);
+});
+
 const changeUser = newUser => {
   console.log("changing name!");
   username = newUser;
   localStorage.removeItem("tracks");
-  localStorage.setItem("username", username);
+
   loadedImages = [];
   trackList = [];
   newTracksToAdd = [];
@@ -161,10 +224,18 @@ const changeUser = newUser => {
   fetchNow();
 };
 
+function getScrollPercent() {
+  var h = document.documentElement,
+    b = document.body,
+    st = "scrollTop",
+    sh = "scrollHeight";
+  return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight);
+}
+
 if (username && trackList.length > 0) {
   console.log("we got username");
   inputEl.value = username;
-  // fetchNow();
-  loadIntoMemory().then(() => render());
+  fetchNow();
+  // loadIntoMemory().then(() => render());
   setStatus("Checking recently played tracks.");
 }
